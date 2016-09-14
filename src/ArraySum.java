01487b148b0c4316de5bf52835cd9c29b3f908d6
cheruku <cheruku.vignan@gmail.com>
2016-09-14 12:32:35 -0400

    Finding simple array sum

---
+++ src/ArraySum.java
@@ -0,0 +1,37 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+public class ArraySum {
+	
+	static int sum(ArrayList<Integer> arrayList){
+		int i, sum=0;
+		for(i=0;i<arrayList.size();i++){
+			sum = sum + arrayList.get(i);
+		}
+		return sum;
+	}
+	
+
+	public static void main(String args[]){
+		
+	//Implementation for to calculate sum for given n number	
+	System.out.println("How many integers would you like to add ");
+	Scanner numSc = new Scanner(System.in);
+	int num = numSc.nextInt();
+	ArrayList<Integer> arraylist = new ArrayList<Integer>();	
+	
+	System.out.println("please insert number of Integers for Sum ");
+	Scanner numSc1 = new Scanner(System.in);
+	while(arraylist.size()<=num-1)
+	{
+	 int i = numSc1.nextInt();
+	 arraylist.add(i);  
+	}
+	
+	int total= sum(arraylist);
+	System.out.println("Total is "+total);
+
+	numSc1.close();
+
+}
+}
